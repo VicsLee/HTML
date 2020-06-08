a7d495ba81bcf00ee0c30c4c0fa3b6df4f7f7e63
VicsLee <59087251+VicsLee@users.noreply.github.com>
2020-06-08 20:15:59 +0300

    Add files via upload

---
+++ projekto_aplankas/index.php
@@ -0,0 +1,40 @@
+<?php
+    $subjects = [
+        'Klase',
+        'Kodas',
+        'Vardas',
+        'Pavarde',
+        'Kontroliniu darbu vidurkis',
+        'Duomenu formavimo data'
+    ];
+
+?>
+
+<<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<table>
+    <tr>
+    <td>Klase</td>
+    <td>Kodas</td>
+    <td>Vardas</td>
+    <td>Pavarde</td>
+    <td>Kontroliniu darbu vidurkis</td>
+    <td>Duomenu formavimo data</td>
+    </tr>
+    <tr>
+
+    </tr>
+    <tr>
+
+    </tr>
+</table>
+</body>
+</html>
